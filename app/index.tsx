442fdf0dda6b78838eac7cd10162f9523435efa7
Aganepes <aaganepes@gmail.com>
2026-01-02 15:33:35 +0500

    init

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function index() {
+	return (
+		<View style={{margin:20}}>
+			<Text style={{fontSize:20,color:"green"}}>Hello React Native!</Text>
+		</View>
+	)
+}
+
+export default index
