8e9af6efcd12b42bcc904299ced13a052ef7fa87
Aganepes <aaganepes@gmail.com>
2026-01-03 10:54:11 +0500

    add to blurRadius to lesson 1.3

--- Lessons/1.Temel UI Bileşenleri/3.Image, ImageBackground/index.tsx
+++ Lessons/1.Temel UI Bileşenleri/3.Image, ImageBackground/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Image, ImageBackground, Text } from 'react-native'
+import React from 'react';
+import { Image, ImageBackground, Text } from 'react-native';
 import img from '../assets/images/react-logo.png';
 function Index() {
 	return (
@@ -9,7 +9,9 @@ function Index() {
 			<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, necessitatibus mollitia deleniti asperiores ratione laborum nostrum blanditiis illo molestias accusantium facere possimus dolores similique libero aut animi? Quaerat, illo molestias?</Text>
 			<Image
 				source={{ uri: "https://www.turkmenistan.gov.tm/storage/app/media/Images/2025/02-2025/05022025/OFFICIAL/06-02-002.jpg" }}
-				width={200} height={400} />
+				width={200} height={400} 
+				blurRadius={5}
+				/>
 			<Image source={img} width={200} height={200} />
 		</ImageBackground>
 	)
